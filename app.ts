c0ffcf63906716e9c2d00bf5605051e8ff4069a0
huzaifamalik0 <163676761+huzaifamalik0@users.noreply.github.com>
2024-03-23 21:02:13 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,4 @@
+let personName = "huzaifa malik"
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.replace(/\b\w/g,(char) => char.toUpperCase()));
